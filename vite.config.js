4d3495d769eee6bcc5996b41612976821ba7d96d
Matt Saemrow <mattsaemrow@gmail.com>
2024-08-16 16:25:06 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    open: true,
+  },
   test: {
     environment: "jsdom",
     globals: true,
