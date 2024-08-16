702525b4fbb4b42b18c4476822f91d6377e9956c
Matt Saemrow <mattsaemrow@gmail.com>
2024-08-16 16:39:49 -0500

    new env variable

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+const BASE_URL = process.env.API_URL || "http://localhost:3001";
 
 /** API Class.
  *
